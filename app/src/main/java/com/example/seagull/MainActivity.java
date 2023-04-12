28692a23bfd4917081eac4aa7627d677afe182d9
Cem <cemozsumer@gmail.com>
2023-04-12 13:32:04 -0400

    Working version of tabs, with empty activities

--- app/src/main/java/com/example/seagull/MainActivity.java
+++ app/src/main/java/com/example/seagull/MainActivity.java
@@ -4,54 +4,92 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-    Button switchToSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        switchToSecondActivity = findViewById(R.id.activity_main_button);
+        //CREATE TAB HOST
+        TabHost tabs = (TabHost) findViewById(R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec;
 
-        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+        //INITIALIZE TAB 1 - MONTHLY
+        spec = tabs.newTabSpec("tag1");   //CREATE NEW TAB SPECIFICATION
+        spec.setContent(R.id.tab1);           //add tab view content
+        spec.setIndicator("Monthly");         //TAB DISPLAY
+        tabs.addTab(spec);                    //put tab in TabHost container
+
+        //TAB 1 CHANGE LISTENER
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
-            public void onClick(View view) {
-                switchActivities();
+            public void onTabChanged(String tabId) {
+                if (tabId.equals("tag1")) {
+                    // START NEW INTENT FOR MONTHLY TAB
+                    Intent intent = new Intent(getApplicationContext(), Monthly.class);
+                    startActivity(intent);
+                }
             }
         });
-    }
 
-    private void switchActivities() {
-        Intent switchActivityIntent = new Intent(this, SecondActivity.class);
-        startActivity(switchActivityIntent);
-    }
+        //INITIALIZE TAB 2 - GOALS
+        spec = tabs.newTabSpec("tag2");   //CREATE NEW TAB SPECIFICATION
+        spec.setContent(R.id.tab2);           //add tab view content
+        spec.setIndicator("Goals");         //TAB DISPLAY
+        tabs.addTab(spec);                    //put tab in TabHost container
 
-    public static class SecondActivity extends AppCompatActivity {
-        Button backButton = findViewById(R.id.activity_second_button);
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_second);
-
-            backButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    finish();
+        //TAB 2 CHANGE LISTENER
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if (tabId.equals("tag2")) {
+                    // START NEW INTENT FOR GOALS TAB
+                    Intent intent = new Intent(getApplicationContext(), Goals.class);
+                    startActivity(intent);
                 }
-            });
-        }
+            }
+        });
+
+        //INITIALIZE TAB 3 - REVIEW
+        spec = tabs.newTabSpec("tag3");   //CREATE NEW TAB SPECIFICATION
+        spec.setContent(R.id.tab3);           //add tab view content
+        spec.setIndicator("Review");         //TAB DISPLAY
+        tabs.addTab(spec);                    //put tab in TabHost container
+
+        //TAB 3 CHANGE LISTENER
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if (tabId.equals("tag3")) {
+                    // START NEW INTENT FOR REVIEW TAB
+                    Intent intent = new Intent(getApplicationContext(), Review.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        //INITALIZE TAB 4 - MAP
+        spec = tabs.newTabSpec("tag4");   //CREATE NEW TAB SPECIFICATION
+        spec.setContent(R.id.tab4);           //add tab view content
+        spec.setIndicator("Map");         //TAB DISPLAY
+        tabs.addTab(spec);                    //put tab in TabHost container
+
+        //TAB 4 CHANGE LISTENER
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if (tabId.equals("tag2")) {
+                    // START NEW INTENT FOR MAP TAB
+                    Intent intent = new Intent(getApplicationContext(), Map.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 }
-
-
-//The financial map will use Google Maps to highlight financial institutions such as banks and ATMs.
-//
-//For each location the user would be able to press on the location to email, call, or reach the website of the institution.
-//On open, pull location data to find local ATM and Bank.
-//Permissions to make phone calls, emails, and open the website for the bank
-
