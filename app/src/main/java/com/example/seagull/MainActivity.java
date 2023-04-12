13b239697885e88dfb2fe2dadc9c7bbfc11064d0
Jack Foster <jackalanfoster01@gmail.com>
2023-04-12 13:33:20 -0400

    help

--- app/src/main/java/com/example/seagull/MainActivity.java
+++ app/src/main/java/com/example/seagull/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.seagull;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button switchToSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToSecondActivity = findViewById(R.id.activity_main_button);
+
+        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, SecondActivity.class);
+        startActivity(switchActivityIntent);
+    }
+
+    public static class SecondActivity extends AppCompatActivity {
+        Button backButton = findViewById(R.id.activity_second_button);
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
     }
 }
+
+
+//The financial map will use Google Maps to highlight financial institutions such as banks and ATMs.
+//
+//For each location the user would be able to press on the location to email, call, or reach the website of the institution.
+//On open, pull location data to find local ATM and Bank.
+//Permissions to make phone calls, emails, and open the website for the bank
+
